ecbad378bd300ad7e6adf12a9cec41b2ed85e05e
Martin <mmaina290@gmail.com>
2023-09-26 19:58:50 +0300

    cOMPLETE

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet() {
   // Write your code here!
+  let distances;
+  if (distances = 199) {
+    return 'This one is on me!';
+  }
+  else if (distances <= 400) {
+    return 'This one is on me!';
+  }
+  else if (distances >= 2001) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else {
+    return 'No can do.';
+  }
 }
+
+
 
 function ternaryCheckCity(){
   // Write your code here!
+  let city;
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  let tip;
+ switch (tip) {
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    default:
+      return "Bye."
+ }
 }
+
+javascript
+console.log(switchOnCharmFromTip('generous')); 
+console.log(switchOnCharmFromTip('not as generous')); 
+console.log(switchOnCharmFromTip('if anything else')); 
